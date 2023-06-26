08e0123bf29a53266817ae404a41b3dd5b2c62e2
igatecomputereducation <igatecomputereducation@gmail.com>
2023-06-26 08:49:17 +0530

    v1

---
+++ Prog168.c
@@ -0,0 +1,34 @@
+import java.util.*;
+class Prog168
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=0,b=0,c=0;
+		try
+		{
+			System.out.print("Num:");
+			a=sc.nextInt();
+			System.out.print("Den:");
+			b=sc.nextInt();
+			c=a/b;
+			System.out.println("Division:"+c);
+		}
+		catch(ArithmeticException ae)
+		{
+			System.out.println(ae.getMessage());
+			System.out.print("Enter a non-zero Den:");
+			b=sc.nextInt();
+			try{
+				c=a/b;
+				System.out.println("Division:"+c);
+			}
+			catch(ArithmeticException ie)
+			{
+				System.out.println(" :-( Go to hell..");
+			}
+
+		}
+		System.out.println("End of main");
+	}
+}
